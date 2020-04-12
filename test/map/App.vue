cfd403af715e1565d01a2f3d241d2f81e472a8e0
oxgos <122486975@qq.com>
2020-04-12 17:36:35 +0800

    添加百度类方法

--- test/map/App.vue
+++ test/map/App.vue
@@ -5,12 +5,18 @@
 </template>
 
 <script>
+import { BaiduMap } from '../../dist/mixMap'
+
 export default {
   name: 'app',
   data() {
     return {
       msg: 'hello world'
     }
+  },
+  created() {
+    let pt = BaiduMap.getPoint()
+    console.log('BaiduMap ' + pt)
   }
 }
 </script>

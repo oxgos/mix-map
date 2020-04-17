6663b76b9d493cf19eca10e3de05accf7db99419
oxgos <122486975@qq.com>
2020-04-17 23:59:39 +0800

    测试已写好方法

--- test/map/App.vue
+++ test/map/App.vue
@@ -1,25 +1,57 @@
 <template>
   <div id="app">
-    {{ msg }}
+    <div class="map__wrapper">
+      <div id="map" ref="map"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import { BaiduMap } from '../../dist/mixMap'
+// import { BaiduMap } from '../../dist/mixMap'
+import { BaiduMap } from '../../lib'
+
+const BEIJING_BD09 = {
+  lng: 116.404,
+  lat: 39.915
+}
 
 export default {
   name: 'app',
   data() {
     return {
-      msg: 'hello world'
+      map: null
     }
   },
-  created() {
-    let pt = BaiduMap.getPoint()
-    console.log('BaiduMap ' + pt)
+  mounted() {
+    this.initMap()
+  },
+  methods: {
+    initMap() {
+      this.map = new BaiduMap('map')
+      this.map.centerAndZoom(BEIJING_BD09, 14)
+      this.map.setMarker(BEIJING_BD09)
+
+      BaiduMap.getAddress(BEIJING_BD09).then(res => {
+        const addr = res
+        console.log('地址:', addr)
+      })
+      const BEIJING_WGS84 = BaiduMap.tranformBD09toWgs84(BEIJING_BD09)
+      console.log('BEIJING_WGS84', BEIJING_WGS84)
+    }
   }
 }
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  .map__wrapper {
+    position: relative;
+  }
+  #map {
+    width: 500px;
+    height: 400px;
+  }
 </style>
--- test/webpack.dev.config.js
+++ test/webpack.dev.config.js
@@ -21,6 +21,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
   },
+  watch: true,
   module: {
     rules: [
       {
